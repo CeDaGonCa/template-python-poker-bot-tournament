32115b50e90a80fca09aeb55352696049a0daea1
Joey6706 <atiejoey68@gmail.com>
2025-02-08 15:35:41 -0500

    hi

--- main.py
+++ main.py
@@ -16,34 +16,27 @@ parser.add_argument('--host', type=str, default='localhost',
 parser.add_argument('--room', type=str, default='my-new-room',
                     help='The room to connect to')
 parser.add_argument('--username', type=str, default='bot',
-                    help='The username for this bot (make sure it\'s unique)')
+                    help='The username to use')
 
 args = parser.parse_args()
 
-cnt = 0
 # Always call
 class TemplateBot(Bot):
     def act(self, state, hand):
         print('asked to act')
         print('acting', state, hand, self.my_id)
-        return {'type': 'call'}
+        return {'type': 'fold'}
 
     def opponent_action(self, action, player):
-        #print('opponent action?', action, player)
-        pass
+        print('opponent action?', action, player)
 
     def game_over(self, payouts):
-        global cnt
-        #print('game over', payouts)
-        cnt += 1
-        print(cnt)
+        print('game over', payouts)
 
     def start_game(self, my_id):
         self.my_id = my_id
-        pass
+        print('start game', my_id)
 
 if __name__ == "__main__":
     bot = TemplateBot(args.host, args.port, args.room, args.username)
     asyncio.run(bot.start())
-
-# awdwda
